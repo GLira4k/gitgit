674cd66cf4d05330554daf43b8a3cf2b47c03d41
Gabriel Lira <gxavierlira@gmail.com>
2025-07-29 16:51:42 -0300

    alter

--- src/pages/ReposPage/index.jsx
+++ src/pages/ReposPage/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Container, Sidebar, Main} from './styles';
 
 function ReposPage() {
     return(
-        <>
-            <h1>Repos Page</h1>
-            <h2>Repositórios do usuário</h2>
-        </>
+        <Container>
+            <Sidebar>
+                <h1>Sidebar</h1>
+            </Sidebar>
+            <Main>
+                <h1>Main</h1>
+            </Main>
+        </Container>
     )
 }
 
